b438bed46f2c041df93f70e25945944f0cc6f3fc
HexallioNN <68469857+HexallioNN@users.noreply.github.com>
2025-11-08 15:25:24 +0100

    first commit

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikalach <ikalach@student.codam.nl>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/08 14:31:47 by ikalach           #+#    #+#             */
+/*   Updated: 2025/11/08 14:33:33 by ikalach          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif /* GET_NEXT_LINE_H */
